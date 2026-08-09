4817699ee07be3d5c3b04f863b1290f02f976407
Ron Lavi <0534306880ron@gmail.com>
2026-08-09 18:01:03 +0300

    שיחות זום בחבילה + פופ-אפ לידים לוואטסאפ

---
+++ assets/navy-D2zzWV3m.js
@@ -0,0 +1,1 @@
+import{j as e,I as n}from"./index-BQcX6tU_.js";function a(){return e.jsx(n,{navy:!0})}export{a as component};
--- assets/navy-p_NaPACC.js
+++
@@ -1,1 +0,0 @@
-import{j as e,I as n}from"./index-8v5o6HY1.js";function a(){return e.jsx(n,{navy:!0})}export{a as component};
