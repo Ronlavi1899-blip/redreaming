3d5c39fc2c029cc7e1e4bb369a984b2ea4fa7c64
Ron Lavi <0534306880ron@gmail.com>
2026-08-09 18:27:52 +0300

    פופ-אפ טופס טלפון + שיחות זום

--- assets/navy-D2zzWV3m.js
+++
@@ -1,1 +0,0 @@
-import{j as e,I as n}from"./index-BQcX6tU_.js";function a(){return e.jsx(n,{navy:!0})}export{a as component};
---
+++ assets/navy-DGBZC2Oz.js
@@ -0,0 +1,1 @@
+import{j as e,I as n}from"./index-C0pfxcL2.js";function a(){return e.jsx(n,{navy:!0})}export{a as component};
