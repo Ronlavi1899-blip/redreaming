f2953bfa1d656f0b2860a089c9121ef8ffa04a7b
Ron Lavi <0534306880ron@gmail.com>
2026-08-11 12:00:19 +0300

    המבנה הדו-מסלולי: ליווי אישי מרכזי + עצמאי 247

---
+++ assets/navy-C-mRr9mo.js
@@ -0,0 +1,1 @@
+import{j as e,I as n}from"./index-CRKLVv6a.js";function a(){return e.jsx(n,{navy:!0})}export{a as component};
--- assets/navy-DGBZC2Oz.js
+++
@@ -1,1 +0,0 @@
-import{j as e,I as n}from"./index-C0pfxcL2.js";function a(){return e.jsx(n,{navy:!0})}export{a as component};
