c96f91cc54c037c05f0bcfe3f0ef9e0d798c250f
Ron Lavi <0534306880ron@gmail.com>
2026-08-11 12:32:42 +0300

    לינק 249 חדש + ניסוח ליווי

--- assets/navy-C-mRr9mo.js
+++
@@ -1,1 +0,0 @@
-import{j as e,I as n}from"./index-CRKLVv6a.js";function a(){return e.jsx(n,{navy:!0})}export{a as component};
---
+++ assets/navy-QyX-IxI8.js
@@ -0,0 +1,1 @@
+import{j as e,I as n}from"./index-EliOlYrF.js";function a(){return e.jsx(n,{navy:!0})}export{a as component};
